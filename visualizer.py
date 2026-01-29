f0dc6530cd1fb077ce0c1854bdd10fe906ac05ee
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-29 20:05:40 +0100

    Update visualizer - adding time

--- visualizer.py
+++ visualizer.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 angles = np.load("angle_log.npy")
 angles = (angles[688:] / 360) * 2 * np.pi
 
-plt.plot(angles)
+step = 0.01
+t = np.arange(0, len(angles)*step, step)
+
+plt.plot(t, angles)
 plt.grid()
 plt.show()
